e4f59ca0366c61261d96d62a0948923fba730690
gocam001 <gocam001@ucr.edu>
2018-04-03 11:41:36 -0700

    Finalized SolarMO code

--- SolarMOmain.ino
+++ SolarMOmain.ino
@@ -1,9 +1,16 @@
 #include <Arduino_FreeRTOS.h>
 #include <Tasks.h>
+#include <PIDTask.h>
 
 void TaskPing(void *pvParameters);
 void TaskBotRandom(void *pvParameters);
-//void TaskPID(void *pvParameters);
+void TaskGetSpeed(void *pvParameters);
+void TaskPID(void *pvParameters);
+void TaskSetSpeed(void *pvParameters);
+void TaskLawn(void *pvParameters);
+void TaskBluetooth(void *pvParameters);
+void TaskServo(void *pvParameters);
+void TaskUI(void *pvParameters);
 
 void setup() {
 
@@ -14,7 +21,14 @@ xTaskCreate (
     , NULL
     , 2 //Priority
     , NULL);
-
+xTaskCreate (
+  TaskUI
+    , (const portCHAR *) "UI"
+    , 128 //Stack size
+    , NULL
+    , 1 //Priority
+    , NULL);
+    
 xTaskCreate (
   TaskBotRandom
     , (const portCHAR *) "Bot"
@@ -22,8 +36,57 @@ xTaskCreate (
     , NULL
     , 1 //Priority
     , NULL);
-  
+
+xTaskCreate (
+  TaskServo
+    , (const portCHAR *) "Servo"
+    , 128 //Stack size
+    , NULL
+    , 1 //Priority
+    , NULL);
+
+xTaskCreate (
+  TaskGetSpeed
+    , (const portCHAR *) "Bot"
+    , 128 //Stack size
+    , NULL
+    , 2 //Priority
+    , NULL);
+
+xTaskCreate (
+  TaskPID
+    , (const portCHAR *) "Bot"
+    , 128 //Stack size
+    , NULL
+    , 2 //Priority
+    , NULL);
+
+xTaskCreate (
+  TaskSetSpeed
+    , (const portCHAR *) "Bot"
+    , 128 //Stack size
+    , NULL
+    , 2 //Priority
+    , NULL);
+ 
+xTaskCreate (
+  TaskLawn
+    , (const portCHAR *) "Ping"
+    , 128 //Stack size
+    , NULL
+    , 2 //Priority
+    , NULL);
+
+xTaskCreate (
+  TaskBluetooth
+    , (const portCHAR *) "Bluetoth"
+    , 128 //Stack size
+    , NULL
+    , 2 //Priority
+    , NULL);
+
 }
+
 
 void loop() {
 }
@@ -40,6 +103,7 @@ void TaskPing(void *pvParameters) //This is a task
   }
 }
 
+
 void TaskBotRandom(void *pvParameters) //This is a task
 {
   (void) pvParameters;
@@ -50,4 +114,92 @@ void TaskBotRandom(void *pvParameters) //This is a task
     rBOT_Tick();
     vTaskDelay(4); //portTICK_PERIOD_MS
   }
+}
+
+
+void TaskServo(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  Servo_Init();
+
+  for (;;) {
+    Servo_Tick();
+    vTaskDelay(1); //portTICK_PERIOD_MS
+  }
+}
+
+void TaskUI(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  UI_Init();
+
+  for (;;) {
+    UI_Tick();
+    vTaskDelay(4); //portTICK_PERIOD_MS
+  }
+}
+
+void TaskGetSpeed(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  getSpeed_Init();
+
+  for (;;) {
+    getSpeed_Tick();
+    vTaskDelay(2); //portTICK_PERIOD_MS
+  }
+}
+
+void TaskPID(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  PID_Init();
+
+  for (;;) {
+    PID_Tick();
+    vTaskDelay(2); //portTICK_PERIOD_MS
+  }
+}
+
+void TaskSetSpeed(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  setSpeed_Init();
+
+  for (;;) {
+    setSpeed_Tick();
+    vTaskDelay(2); //portTICK_PERIOD_MS
+  }
+  
+}
+
+void TaskLawn(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  Lawn_Init();
+
+  for (;;) {
+    Lawn_Tick();
+    vTaskDelay(4); //portTICK_PERIOD_MS
+  }
+  
+}
+
+void TaskBluetooth(void *pvParameters) //This is a task
+{
+  (void) pvParameters;
+
+  Bluetooth_Init();
+
+  for (;;) {
+    Bluetooth_Tick();
+    vTaskDelay(4); //portTICK_PERIOD_MS
+  }
+  
 }
